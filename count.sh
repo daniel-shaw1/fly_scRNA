cf35263a0c9e62f69876ba3c053e2a53e03a4ca7
Daniel Shaw <43077619+daniel-shaw1@users.noreply.github.com>
2025-07-31 15:46:25 -0600

    Update count.sh

--- count.sh
+++ count.sh
@@ -11,13 +11,14 @@
 #SBATCH --error=counts.%j.err
 
         ## Command(s) to run:
+        ###change path to your desired output folder
 cd /mnt/beegfs/scratch/ds221041e/fly/
 
 export PATH=~/software/cellranger-9.0.1/:$PATH
 
 cellranger count --id=samplelib1 \
-           --transcriptome=/mnt/beegfs/scratch/ds221041e/fly/cellrangerfly_genome/ \
-           --fastqs=/mnt/beegfs/scratch/bl180922/data/01.RawData/Lib_1/ \
+           --transcriptome=/mnt/beegfs/scratch/ds221041e/fly/cellrangerfly_genome/ \ ###path to where the mkref output directory is on your folder
+           --fastqs=/mnt/beegfs/scratch/bl180922/data/01.RawData/Lib_1/ \    ###path to where the fastq reads are
            --sample=Lib_1-SI_TT_A1_22V5MTLT4 \
            --create-bam=true \
            --localcores=8 \
